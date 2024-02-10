defa8d3fd81b3322c79d27b4e19e87228403a9e8
ecruosnepo <67457511+ecruosnepo@users.noreply.github.com>
2024-02-11 03:12:01 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 148 ms, Memory: 16128 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -9,16 +9,33 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        int abmul = a*b;
-        int minmul = 1;
-        int maxdiv = Integer.MIN_VALUE;
+//        int abmul = a*b;
+//        int minmul = 1;
+//        int maxdiv = Integer.MIN_VALUE;
+//
+//        for (int i = 1; i <= Math.min(a,b); i++) {
+//            if(a%i==0 && b%i==0 && maxdiv < i){
+//                maxdiv = i;
+//            }
+//        }
+//        minmul = abmul/maxdiv;
+//        System.out.println(maxdiv + "\n" + minmul);
 
-        for (int i = 1; i <= Math.min(a,b); i++) {
-            if(a%i==0 && b%i==0 && maxdiv < i){
-                maxdiv = i;
+        //유클리드 호제법
+        int big = Math.max(a,b);
+        int small = Math.min(a,b);
+        int remain = 0;
+        do{
+            remain = big%small;
+            if(remain == 0){
+                remain = small;
+                break;
             }
-        }
-        minmul = abmul/maxdiv;
-        System.out.println(maxdiv + "\n" + minmul);
+            big = small;
+            small = remain;
+
+        }while(big%small != 0);
+
+        System.out.println(remain + "\n" + a*b/small);
     }
 }
